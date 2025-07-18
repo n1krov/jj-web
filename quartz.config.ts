12f5357765495260de1bdaef3506d713a26893e3
Z0SO <lautarorobales31@gmail.com>
2025-07-18 19:54:47 -0300

    update qartz config

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "es-AR",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
